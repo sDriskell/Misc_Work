fab56f03f5270602ea6247b24478f1c369495b8f
sDriskell <Driskell.Shane.L@gmail.com>
2020-11-09 13:46:52 -0500

    Able to run N amount of games per user's input and track total scores.

--- Auto2048.py
+++ Auto2048.py
@@ -8,24 +8,13 @@ Requires geckodriver to added to your PATH.
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#TODO: write function for inputing keystrokes
-#TODO: check to see if game is over
-#TODO: start new game if game is over
-#TODO: track high score
+URL = "https://play2048.co/"
 
 
-def main():
-    url = "https://play2048.co/"
-
-    browser = webdriver.Firefox()
-    browser.get(url)
-
+def auto_input(browser: webdriver.Firefox) -> int:
     html = browser.find_element_by_tag_name('html')
-
     retry_button = browser.find_element_by_class_name("retry-button")
     new_game_button = browser.find_element_by_class_name("restart-button")
-    print(type(retry_button))
-    print(type(new_game_button))
 
     while True and not retry_button.is_displayed():
         html.send_keys(Keys.UP)
@@ -33,8 +22,24 @@ def main():
         html.send_keys(Keys.LEFT)
         html.send_keys(Keys.RIGHT)
 
+    score = browser.find_element_by_class_name("score-container").text
     retry_button.click()
     new_game_button.click()
+    return score
+
+
+# TODO: track high score
+
+
+def main():
+    browser = webdriver.Firefox()
+    browser.get(URL)
+
+    total_score = []
+    games = int(input("How many times do you want to play? "))
+    for i in range(0, games):
+        total_score.append(auto_input(browser))
+    print(total_score)
 
 
 if __name__ == '__main__':
